ee12b9e65f94408b7ee54dd00ff5bc8d44b29f04
iPegii <51372604+iPegii@users.noreply.github.com>
2022-05-05 15:58:28 +0300

    add searchbar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+
+ const SearchBar = ({hashtags, handleSearchBar}) => {
+     console.log(hashtags)
+     const filteredHashtags = hashtags.map(h => {return h.hashtag})
+     console.log(filteredHashtags)
+  return (
+    <Autocomplete
+      disablePortal
+      id="combo-box-demo"
+      style={{backgroundColor: "white",display: "grid", margin: "auto"}}
+      options={filteredHashtags}
+      onInputChange={(event,value) =>handleSearchBar(value)}
+      sx={{ width: 300 }}
+      renderInput={(params) => <TextField {...params} label="Movie" />}
+    />
+  );
+}
+
+
+export default SearchBar
--- src/components/ShowStuff.js
+++ src/components/ShowStuff.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import SearchBar from './SearchBar'
 
 const App = () => {
     const [ hashtags, setHashtags ] = useState([])
     const [ activities, setActivities ] = useState([])
     const [ users, setUsers ] = useState([])
+    const [searchBar, setSearchBar] = useState([])
     
 
     
@@ -13,31 +15,31 @@ const App = () => {
           axios
           .get('https://hackathlon.nitorio.us/users')
           .then(response => {
+            
             setUsers(response.data)
-            axios
-            .get('https://hackathlon.nitorio.us/activities')
-            .then(response => {
-              let newActivities = []
-              response.data.map(a =>  {          
-                let tempActivity = a
-                users.map(u => {
-                  if(tempActivity.id === u.activityId)
-                  if(tempActivity.personsIn) {
-                    tempActivity = {...tempActivity, personsIn: tempActivity.personsIn+1}
-                  } else {
-                    tempActivity = {...tempActivity, personsIn: 1}
-                  }
-                  return tempActivity
-                  }
-                )
-                console.log(tempActivity)
-                newActivities = newActivities.concat(tempActivity)
-           return 1  
-          }
-          )
-            console.log(newActivities)
-              setActivities(newActivities)
-            })
+          })
+
+          axios
+          .get('https://hackathlon.nitorio.us/activities')
+          .then(response => {
+            let newActivities = []
+            response.data.map(a =>  {          
+              let tempActivity = a
+              users.map(u => {
+                if(tempActivity.id === u.activityId)
+                if(tempActivity.personsIn) {
+                  tempActivity = {...tempActivity, personsIn: tempActivity.personsIn+1}
+                } else {
+                  tempActivity = {...tempActivity, personsIn: 1}
+                }
+                return tempActivity
+                }
+              )
+              newActivities = newActivities.concat(tempActivity)
+         return 1  
+        }
+        )
+            setActivities(newActivities)
           })
 
           axios
@@ -48,12 +50,19 @@ const App = () => {
 
 
       }
-      useEffect(hook, [users])
-      
+      useEffect(hook, [])
+
+      const handleSearchBar = (value) => {
+        setSearchBar(value)
+        console.log(value)
+      }
+     const hashtagsFiltered = hashtags.filter(h => h.hashtag === searchBar)
     return(
-    <div>
+    <div style={{display: "grid", margin: "auto"}}>
         <h1>NINTRESSIT</h1>
-        {hashtags.map((h,index) => {
+        <h2>{searchBar}</h2>
+        <SearchBar handleSearchBar={handleSearchBar} searchBar={searchBar} hashtags={hashtags} style={{margin: "auto"}}></SearchBar>
+        {hashtagsFiltered.map((h,index) => {
           return (<div key={index}>
              <p>{h.hashtag}</p>
              {console.log(h.people)}
